29fbced67d1d8d0ae9f4fa3171d8ed8212a9368a
Түвшинжаргал <53771399+atuvshinjargal@users.noreply.github.com>
2022-08-22 19:02:01 +0800

    last

--- lib/stopwatch.dart
+++ lib/stopwatch.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:myapp/main.dart';
 
 class StopWatch extends StatefulWidget {
   const StopWatch({Key? key}) : super(key: key);
@@ -26,16 +27,62 @@ class _StopWatchState extends State<StopWatch> {
   }
 
   @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Stopwatch'),
       ),
-      body: Center(
-        child: Text(
-          '$seconds секунд',
-          style: Theme.of(context).textTheme.headline5,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Text(
+              '$seconds секунд',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: null,
+                child: Text('Эхлэх'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Colors.green,
+                  ),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                    Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              TextButton(
+                onPressed: null,
+                child: Text('Дуусгах'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Colors.red,
+                  ),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                    Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
